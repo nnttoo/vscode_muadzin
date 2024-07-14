ef570cd48fa787ddada2ac942f6966d64b65d906
Anto <aaaannnnttttoooo@gmail.com>
2024-07-14 15:53:10 +0800

    Add : done hearbeat

--- src/extenstion_heartbeat.ts
+++ src/extenstion_heartbeat.ts
@@ -1,4 +1,27 @@
 
-export class ExtHeartBeat{
-    
+
+function sleep(milis: number) {
+    return new Promise((r, x) => {
+        setTimeout(r, milis);
+    });
+}
+export class ExtHeartBeat {
+    public keepRun = false;
+
+    public run(){
+        console.log("halo");
+    }
+
+    public async startTimer() {
+        this.keepRun = true;
+        while(this.keepRun){
+            await sleep(1000);
+            try {
+                this.run();
+            } catch (error) {
+                
+            }
+        }
+    }
+
 }
